a7f07038c44c470a97b0c6444cd44486a607bbc8
mispasyuk <maria.spasyuk@gmail.com>
2024-03-17 01:18:57 +0300

    added some fixes

--- polygons.cpp
+++ polygons.cpp
@@ -3,12 +3,12 @@
 #include <list>
 #include <vector>
 #include <algorithm>
+#include <time.h>
 #define border 1000
 #define eps 1E-8
-
 struct Point2d
 {
-	double x, y;
+    double x, y;
 
     bool operator== (Point2d b)
     {
@@ -17,58 +17,67 @@ struct Point2d
 };
 struct Line
 {
-	double a, b, c;
+    double a, b, c;
 };
 struct Segment
 {
-	Point2d start, finish;
+    Point2d start, finish;
 };
 //it is also necessary to check that 3 points do not belong to one line
-std::vector <std::vector <Point2d>> GenerateTriangles(uint32_t numOfTriangles)
+std::vector <std::vector <Segment>> GenerateTriangles(uint32_t numOfTriangles)
 {
-	std::srand(0);
-	std::vector <std::vector <Point2d>> Triangles;
-	for (int i = 0; i < numOfTriangles; ++i)
-	{
-		std::vector <Point2d> tempList;
-		uint32_t vertexes = 3;
-		for (int k = 1; k <= vertexes; ++k)
-		{
-			Point2d temp;
-			temp.x = rand() % (10 - 0 + 1) - 10;
-			temp.y = rand() % (10 - 0 + 1) - 10;
-			tempList.push_back(temp);
-		}
-		Triangles.push_back(tempList);
-	}
-	return Triangles;
+    std::vector <std::vector <Segment>> Triangles;
+    for (int i = 0; i < numOfTriangles; ++i)
+    {
+        std::vector <Point2d> tempList;
+        uint32_t vertexes = 3;
+        std::vector<Segment> segments;
+        for (int k = 1; k <= vertexes; ++k)
+        {
+            Point2d temp;
+            temp.x = rand() % (10 - 0 + 1) - 10;
+            temp.y = rand() % (10 - 0 + 1) - 10;
+            tempList.push_back(temp);
+        }
+        for (int j = 0; j < vertexes; ++j)
+        {
+            Segment segment;
+            segment.start = tempList.at(j);
+            segment.finish = tempList.at((j + 1) % vertexes);
+            segments.push_back(segment);
+        }
+        Triangles.push_back(segments);
+
+    }
+    return Triangles;
+}
+int distance(Point2d p0, Point2d p1) 
+{
+    int x_span = p0.x - p1.x;
+    int y_span = p0.y - p1.y;
+    return y_span * y_span + x_span * x_span;
 }
 // check that start < finish for segments
 void IntersectionOfSegments(Segment s1, Segment s2, std::vector <Point2d>& vertex)
 {
-	Line l1 = { s1.start.y - s1.finish.y, s1.finish.x - s1.start.x, l1.a * s1.start.x + l1.b * s1.start.y };
-	Line l2 = { s2.start.y - s2.finish.y, s2.finish.x - s2.start.x, l2.a * s2.start.x + l2.b * s2.start.y };
-	double check = l1.a * l2.b - l1.b * l2.a;
-	if (abs(check) < eps)
-	{
-		return;
-	}
-	Point2d commonPoint = { (l1.c * l2.b  - l2.c * l1.b ) / check, (l1.a * l2.c  - l1.c * l2.a ) / check };
-	if (commonPoint.x >= s1.start.x and commonPoint.x <= s1.finish.x and commonPoint.x >= s2.start.x and commonPoint.x <= s2.finish.x)
-	{
-		vertex.push_back(commonPoint);
-	};
+    Line l1 = { s1.start.y - s1.finish.y, s1.finish.x - s1.start.x, l1.a * s1.start.x + l1.b * s1.start.y };
+    Line l2 = { s2.start.y - s2.finish.y, s2.finish.x - s2.start.x, l2.a * s2.start.x + l2.b * s2.start.y };
+    double check = l1.a * l2.b - l1.b * l2.a;
+    if (abs(check) < eps)
+    {
+        return;
+    }
+    Point2d commonPoint = { (l1.c * l2.b - l2.c * l1.b) / check, (l1.a * l2.c - l1.c * l2.a) / check };
+    if (sqrt(distance(commonPoint, s1.start)) + sqrt(distance(commonPoint, s1.finish)) - sqrt(distance(s1.start, s1.finish)) < eps
+        && sqrt(distance(commonPoint, s2.start)) + sqrt(distance(commonPoint, s2.finish)) - sqrt(distance(s2.start, s2.finish)) < eps)
+    {
+        vertex.push_back(commonPoint);
+    }
 }
 double polar_angle(Point2d p0, Point2d p1) {
     int x_span = p0.x - p1.x;
     int y_span = p0.y - p1.y;
     return atan2(y_span, x_span);
-}
-
-int distance(Point2d p0, Point2d p1) {
-    int x_span = p0.x - p1.x;
-    int y_span = p0.y - p1.y;
-    return y_span * y_span + x_span * x_span;
 }
 
 int det(Point2d p1, Point2d p2, Point2d p3) {
@@ -138,14 +147,16 @@ std::vector<Point2d> scan(std::vector<Point2d>& points) {
 
 int main()
 {
+    std::srand(std::time(NULL));
     int numOfTriangles;
     std::cin >> numOfTriangles;
     std::vector<Point2d> ans;
-    std::vector <std::vector <Point2d>> triangles = GenerateTriangles(numOfTriangles);
-    for (auto triangle : triangles)
+    std::vector <std::vector <Segment>> triangles = GenerateTriangles(numOfTriangles);
+    std::vector<std::vector<Point2d>> segments;
+    /*for (auto triangle : triangles)
     {
         Segment firstSeg, secondSeg;
-        firstSeg.start = triangle.at(0); 
+        firstSeg.start = triangle.at(0);
         firstSeg.finish = triangle.at(1);
         secondSeg.finish = triangle.at(1);
         secondSeg.start = triangle.at(2);
@@ -153,6 +164,33 @@ int main()
     }
     std::cout << "ans = " << ans.at(0).x << " " << ans.at(0).y << "\n";
     std::cout << "vertex = " << triangles.at(0).at(1).x << " " << triangles.at(0).at(1).y << "\n";
+    */
+    //Point2d a, b, c;
+    //a.x = 3;
+    //a.y = 6;
+    //b.x = 0;
+    //b.y = 0;
+    //c.x = 4;
+    //c.y = 8;
+    //std::cout << (sqrt(distance(a, b)) + sqrt(distance(a, c)) == sqrt(distance(b, c)));
+    int i = 0;
+    while (i < triangles.size())
+    {
+        for (int j = 0; j < triangles.at(i).size(); ++j)
+        {
+            int k = i + 1;
+            while (k < triangles.size())
+            {
+                for (int l = 0; l < triangles.at(k).size(); ++l)
+                {
+                    IntersectionOfSegments(triangles.at(i).at(j), triangles.at(k).at(l), ans);
+                }
+                k += 1;
+            }
+        }
+        i += 1;
+    }
+    std::cout << 1;
     /*std::vector<Point2d> res_pts = scan();
     res_pts.push_back(res_pts[0]);
     int p1 = 0;
